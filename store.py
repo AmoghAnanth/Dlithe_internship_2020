77123aef17ccd0d87757b3cf6630510943cafdb5
AmoghAnanth <67820245+AmoghAnanth@users.noreply.github.com>
2020-07-25 01:03:58 +0530

    Assignment 2: Coding used for predicting the revenue earned.

---
+++ store.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 16:48:59 2020
+
+@author: Amogh A N
+"""
+
+import pandas as pd
+import numpy as np
+#reading the file from folder
+data = pd.read_csv("C:/Users/Amogh A N/Desktop/workshop/Python_Dataset/assignment/store.csv")
+#data cleanising
+#drop not so important data
+data = data.iloc[:, np.r_[3,0:3,4]]
+data.info()
+df = pd.get_dummies(data, prefix=['reps', 'product','region'], columns=['reps', 'product','region'])
+#pictorial relation of data/data analysis
+import seaborn as sb
+sb.heatmap(df.corr(),annot=True,vmin=0.5,vmax=0.7,cmap='coolwarm',linewidth=3,linecolor='red')
+sb.pairplot(df)
+
+#setting and x and y arrays
+#iv:reps,product,qty,region
+#tv: revenue
+x = df.iloc[:,1:].values
+y = df.iloc[:,0].values
+
+#split universal dataset(train:test)
+#library:sklearn
+#module:model_selection
+#class : train_test_split
+from sklearn.model_selection import train_test_split as tts
+x_train,x_test,y_train,y_test = tts(x,y,test_size=0.3,random_state=2500)
+#Algorithm selection
+#Linear regression
+#Library: sklearn
+#module : Linear_model
+#class  : LinearRegression
+from sklearn.linear_model import LinearRegression as linreg
+model_linreg = linreg()
+#train the model. Use fit(training arrays)
+model_linreg.fit(x_train,y_train)
+y_pred = model_linreg.predict(x_test)
+#check accuracy by giving x_test and y_test data
+accuracy = model_linreg.score(x_test,y_test)
+print("Linear Regression Accuracy:", accuracy)
+#Checking the accuracy
+#Library: sklearn
+#module : metrics
+#class  : r2score
+#r2score(actual, predicted)
+from sklearn.metrics import r2_score as r2s
+cm = r2s(y_test, y_pred)
